8e5c87e930e19c6be8d972e0eb9e2876ca0a5378
Daulet Zhumadilov <daulet140@gmail.com>
2024-05-02 22:40:17 +0500

    reply_to_message

--- models.go
+++ models.go
@@ -207,8 +207,30 @@ type Message struct {
 		Username  string `json:"username"`
 		Type      string `json:"type"`
 	} `json:"chat"`
-	Date int    `json:"date"`
-	Text string `json:"text"`
+	Date           int             `json:"date"`
+	Text           string          `json:"text"`
+	ReplyToMessage *ReplyToMessage `json:"reply_to_message,omitempty"`
+}
+
+type ReplyToMessage struct {
+	ReplyToMessage struct {
+		MessageId int `json:"message_id"`
+		From      struct {
+			Id           int    `json:"id"`
+			IsBot        bool   `json:"is_bot"`
+			FirstName    string `json:"first_name"`
+			Username     string `json:"username"`
+			LanguageCode string `json:"language_code"`
+		} `json:"from"`
+		Chat struct {
+			Id        int    `json:"id"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+			Type      string `json:"type"`
+		} `json:"chat"`
+		Date int    `json:"date"`
+		Text string `json:"text"`
+	} `json:"reply_to_message"`
 }
 
 type ChatMember struct {
