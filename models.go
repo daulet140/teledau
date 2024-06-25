278d3bffea0007b2b487e1d788e3a5a8810cd50e
DZhumadilov <dzhumadilov@1cb.kz>
2024-06-25 15:26:41 +0500

    GetChatInfo

--- models.go
+++ models.go
@@ -340,17 +340,17 @@ type GetChatResponse struct {
 	GetChat GetChat `json:"result"`
 }
 type GetChat struct {
-	Id                int64     `json:"id"`
-	Title             string    `json:"title"`
-	Username          string    `json:"username"`
-	Type              string    `json:"type"`
-	ActiveUsernames   []string  `json:"active_usernames"`
-	Description       string    `json:"description"`
-	InviteLink        string    `json:"invite_link"`
-	HasVisibleHistory bool      `json:"has_visible_history"`
-	Photo             ChatPhoto `json:"photo"`
-	MaxReactionCount  int       `json:"max_reaction_count"`
-	AccentColorId     int       `json:"accent_color_id"`
+	Id                int64      `json:"id"`
+	Title             string     `json:"title"`
+	Username          string     `json:"username"`
+	Type              string     `json:"type"`
+	ActiveUsernames   []string   `json:"active_usernames"`
+	Description       string     `json:"description"`
+	InviteLink        string     `json:"invite_link"`
+	HasVisibleHistory bool       `json:"has_visible_history"`
+	Photo             *ChatPhoto `json:"photo"`
+	MaxReactionCount  int        `json:"max_reaction_count"`
+	AccentColorId     int        `json:"accent_color_id"`
 }
 
 type ChatPhoto struct {
